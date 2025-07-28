b068d2bae93159f7dcac49cb09b339c0ce37af7b
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-07-28 23:43:04 +0300

    Add helping functions

--- projects/face_anonimizer_video.py
+++ projects/face_anonimizer_video.py
@@ -1,6 +1,32 @@
 import cv2
 import mediapipe as mp
 
+def to_rgb(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def to_bgr(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+
+def get_bbox_values(bbox, H, W, padding=0.01):
+    x1 = int((bbox.xmin - padding * bbox.width) * W)
+    y1 = int((bbox.ymin - padding * bbox.height) * H)
+    w = int((bbox.width + 2 * padding * bbox.width) * W)
+    h = int((bbox.height + 2 * padding * bbox.height) * H)
+    # Ensure coordinates stay within image bounds
+    x1 = max(0, x1)
+    y1 = max(0, y1)
+    w = min(w, W - x1)
+    h = min(h, H - y1)
+
+    return x1, y1, w, h
+
+
+def blur_img_segment(img, x1, y1, w, h, ksize=55):
+    img[y1:y1+h, x1:x1+w, :] = cv2.blur(img[y1:y1+h, x1:x1+w, :], ksize=(ksize, ksize))
+    return img
+
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
