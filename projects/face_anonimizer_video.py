e684206ee5b75efb60466d9f2a5dd74d4b28a039
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-07-29 00:36:31 +0300

    Move common functions to separate directory

--- projects/face_anonimizer_video.py
+++ projects/face_anonimizer_video.py
@@ -1,33 +1,6 @@
 import cv2
 import mediapipe as mp
-
-def to_rgb(img):
-    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
-def to_bgr(img):
-    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-
-def get_bbox_values(bbox, H, W, padding=0.01):
-    print("="*80)
-    print("in the bounding box")
-    x1 = int((bbox.xmin - padding * bbox.width) * W)
-    y1 = int((bbox.ymin - padding * bbox.height) * H)
-    w = int((bbox.width + 2 * padding * bbox.width) * W)
-    h = int((bbox.height + 2 * padding * bbox.height) * H)
-    # Ensure coordinates stay within image bounds
-    x1 = max(0, x1)
-    y1 = max(0, y1)
-    w = min(w, W - x1)
-    h = min(h, H - y1)
-
-    return x1, y1, w, h
-
-
-def blur_img_segment(img, x1, y1, w, h, ksize=55):
-    img[y1:y1+h, x1:x1+w, :] = cv2.blur(img[y1:y1+h, x1:x1+w, :], ksize=(ksize, ksize))
-    return img
+from utils import to_bgr, to_rgb, blur_img_segment, get_bbox_values
 
 
 def process_img(img, face_detection_obj):
@@ -45,7 +18,6 @@ def process_img(img, face_detection_obj):
             # Blur Image
             img_rgb = blur_img_segment(img_rgb, x1, y1, w, h)
     return to_bgr(img_rgb)
-
 
 # Detect the face
 mp_face_detection = mp.solutions.face_detection
--- projects/utils.py
+++ projects/utils.py
@@ -1,22 +1,32 @@
 import cv2
 import numpy as np
 
-# Get the lower and upper limits of a color in HSV space
-# color: list of 3 integers representing the color in BGR space
-# returns: tuple of 2 lists of 3 integers representing the lower and upper limits of the color in HSV space
-def get_limits(color):
-    # Convert to numpy array
-    c = np.uint8([[color]])  # insert the bgr color into an array to convert to hsv
-    # Convert to HSV
-    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-    
-    # Set the lower limit to the same value as the color
-    lower_limit = hsvC[0][0][0] - 10, 100, 100
-    # Set the upper limit to the same value as the color
-    upper_limit = hsvC[0][0][0] + 10, 255, 255
-    
-    # Convert to numpy array
-    lower_limit = np.array(lower_limit, dtype=np.uint8)
-    upper_limit = np.array(upper_limit, dtype=np.uint8)
-    
-    return lower_limit, upper_limit
+def to_rgb(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def to_bgr(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+
+def get_bbox_values(bbox, H, W, padding=0.01):
+    print("="*80)
+    print("in the bounding box")
+    x1 = int((bbox.xmin - padding * bbox.width) * W)
+    y1 = int((bbox.ymin - padding * bbox.height) * H)
+    w = int((bbox.width + 2 * padding * bbox.width) * W)
+    h = int((bbox.height + 2 * padding * bbox.height) * H)
+    # Ensure coordinates stay within image bounds
+    x1 = max(0, x1)
+    y1 = max(0, y1)
+    w = min(w, W - x1)
+    h = min(h, H - y1)
+
+    return x1, y1, w, h
+
+
+def blur_img_segment(img, x1, y1, w, h, ksize=55):
+    img[y1:y1+h, x1:x1+w, :] = cv2.blur(img[y1:y1+h, x1:x1+w, :], ksize=(ksize, ksize))
+    return img
+
+
