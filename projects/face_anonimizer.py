ccf8c3a6b2d4efab1bf93045ae0dfec16d297dba
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-07-29 00:38:01 +0300

    Call helping functions from the utils directory

--- projects/face_anonimizer.py
+++ projects/face_anonimizer.py
@@ -1,23 +1,6 @@
 import cv2
 import mediapipe as mp
-
-def get_bbox_values(bbox, H, W, padding=0.01):
-    x1 = int((bbox.xmin - padding * bbox.width) * W)
-    y1 = int((bbox.ymin - padding * bbox.height) * H)
-    w = int((bbox.width + 2 * padding * bbox.width) * W)
-    h = int((bbox.height + 2 * padding * bbox.height) * H)
-    # Ensure coordinates stay within image bounds
-    x1 = max(0, x1)
-    y1 = max(0, y1)
-    w = min(w, W - x1)
-    h = min(h, H - y1)
-
-    return x1, y1, w, h
-
-
-def blur_img_segment(img, x1, y1, w, h, ksize=55):
-    img[y1:y1+h, x1:x1+w, :] = cv2.blur(img[y1:y1+h, x1:x1+w, :], ksize=(ksize, ksize))
-    return img
+from utils import to_bgr, to_rgb, blur_img_segment, get_bbox_values
 
 
 # Read the image
