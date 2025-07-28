54517aec4539994ce0a82e347963a878c0455f96
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-07-28 20:23:39 +0300

    Add mask for yellow color

--- projects/color_detection.py
+++ projects/color_detection.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from utils import get_limits
 
 yellow = [0, 255, 255]
 
@@ -10,10 +10,19 @@ while True:
     if not ret:
         break
 
-    # Convert to HSV
+    # Convert frame from BGR to HSV
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow("Frame", frame)
+    # Get the lower and upper limits of the color
+    lower_limit, upper_limit = get_limits(yellow)
+
+    # Create a mask of the color
+    mask = cv2.inRange(frame_hsv, lower_limit, upper_limit)
+
+    # Show the mask
+    cv2.imshow("Frame", mask)
+
+    # Break the loop if the user presses the 'q' key
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
