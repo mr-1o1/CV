bd3232b2d2892e238d40c090d9917b60958ea425
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-07-28 22:55:32 +0300

    Divide task into separate functions

--- projects/face_anonimizer.py
+++ projects/face_anonimizer.py
@@ -1,6 +1,25 @@
 import cv2
 import mediapipe as mp
 
+def get_bbox_values(bbox, H, W, padding=0.01):
+    x1 = int((bbox.xmin - padding * bbox.width) * W)
+    y1 = int((bbox.ymin - padding * bbox.height) * H)
+    w = int((bbox.width + 2 * padding * bbox.width) * W)
+    h = int((bbox.height + 2 * padding * bbox.height) * H)
+    # Ensure coordinates stay within image bounds
+    x1 = max(0, x1)
+    y1 = max(0, y1)
+    w = min(w, W - x1)
+    h = min(h, H - y1)
+
+    return x1, y1, w, h
+
+
+def blur_img_segment(img, x1, y1, w, h, ksize=55):
+    img[y1:y1+h, x1:x1+w, :] = cv2.blur(img[y1:y1+h, x1:x1+w, :], ksize=(ksize, ksize))
+    return img
+
+
 # Read the image
 # img_path = "/Users/hanzy/Documents/Projects/py-projects/CV/images/rgb/boy_portrait.png"
 img_path = "/Users/hanzy/Documents/Projects/py-projects/CV/images/rgb/four_people.jpg"
@@ -18,29 +37,14 @@ with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence
     if results.detections:
         print(results.detections)
         for detection in results.detections:
-            location_data = detection.location_data
-            bbox = location_data.relative_bounding_box
-
-            # x1, y1, w, h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
-
-            padding = 0.01  # Add 10% padding to width and height
-            x1 = int((bbox.xmin - padding * bbox.width) * W)
-            y1 = int((bbox.ymin - padding * bbox.height) * H)
-            w = int((bbox.width + 2 * padding * bbox.width) * W)
-            h = int((bbox.height + 2 * padding * bbox.height) * H)
-            # Ensure coordinates stay within image bounds
-            x1 = max(0, x1)
-            y1 = max(0, y1)
-            w = min(w, W - x1)
-            h = min(h, H - y1)
+            x1, y1, w, h = get_bbox_values(detection.location_data.relative_bounding_box, H, W)
 
             # Blur Image
-            img[y1:y1+h, x1:x1+w, :] = cv2.blur(img[y1:y1+h, x1:x1+w, :], ksize=(55, 55))
+            img = blur_img_segment(img, x1, y1, w, h)
 
             # green_color = [0, 255, 0]
             # thickness = 3
             # cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), green_color, thickness=thickness)
-
 
             # mp_drawing.draw_detection(img, detection)
 
