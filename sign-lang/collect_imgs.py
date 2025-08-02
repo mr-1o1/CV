5db87b0bfc622c2a4c901e4646595ad6e6082a02
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-08-02 19:01:50 +0300

    Show current character's data being captured

--- sign-lang/collect_imgs.py
+++ sign-lang/collect_imgs.py
@@ -6,7 +6,7 @@ DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-number_of_classes = 2
+number_of_classes = 26
 dataset_size = 100
 
 cap = cv2.VideoCapture(0)
@@ -14,13 +14,14 @@ for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
-    print('Collecting data for class {}'.format(j))
+    print('Collecting data for class {}'.format(chr(j+65)))
+
+    print(f'For character \'{chr(j+97)}\' - Press "Q" to start ! :)')
 
     done = False
     while True:
         ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
-                    cv2.LINE_AA)
+        cv2.putText(frame, f'For character \'{chr(j+65)}\' - Press "Q" to start ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
         cv2.imshow('frame', frame)
         if cv2.waitKey(25) == ord('q'):
             break
@@ -30,7 +31,8 @@ for j in range(number_of_classes):
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+        cv2.imwrite(os.path.join(DATA_DIR, str(
+            j), '{}.jpg'.format(counter)), frame)
 
         counter += 1
 
