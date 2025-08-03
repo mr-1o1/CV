01f9bf870594f19126bab09c977d79513c65e18f
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-08-03 19:55:29 +0300

    Train the model

--- sign-lang/train_classifier.py
+++ sign-lang/train_classifier.py
@@ -6,6 +6,11 @@ from sklearn.metrics import accuracy_score
 
 import numpy as np
 
+def printp(*args):
+    print("="*80)
+    print(*args)
+    print()
+
 data_dict = pickle.load(open('./data.pickle', 'rb'))
 
 print(data_dict.keys())
@@ -13,4 +18,18 @@ print(data_dict.keys())
 
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
+
+printp("Splitting the data")
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+model = RandomForestClassifier()
+
+printp("Training the data")
+model.fit(x_train, y_train)
+
+printp("Creating predictions")
+y_predict = model.predict(x_test)
+
+score = accuracy_score(y_predict, y_test)
+printp(f"{score*100:0.2F}% of samples were classified correctly!")
 
